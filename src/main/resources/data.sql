151691afd73cbb84c610349f38e5be9546a22170
Todor <todor.r.penchev@gmail.com>
2023-01-05 23:35:55 +0200

    SQL table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,15 @@
-INSERT INTO users(username, password, email, created)
-values ('test', 'pass-test', 'test@test.com', '2022-12-12');
+INSERT INTO users(first_name, fam_name, email, password,  created, id_challengge_compl)
+values ('test', 'testov', 'test@test.com', 'parola123','2022-12-12', 1);
 
+INSERT INTO Accs(username, clearance_lvl)
+values('test_1', 1);
+
+INSERT INTO Clearance(clearance_position)
+values
+        ('user'),
+        ('moderator'),
+        ('admin');
+;
+
+INSERT INTO CH_completed(created_by)
+values('user1');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,10 +1,41 @@
+DROP TABLE IF EXISTS Accs;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS Clearance;
+DROP TABLE IF EXISTS CH_completed;
+CREATE TABLE Accs
+(
+    id_a SERIAL NOT NULL,
+    username varchar(100) NOT NULL,
+    clearance_lvl int NOT NULL
+    PRIMARY KEY (id_a)
+    FOREIGN KEY(clearance_lvl) REFERENCES Clearance(id_c)
+);
 CREATE TABLE users
 (
-    id SERIAL NOT NULL,
-    username varchar(100) NOT NULL,
-    password varchar(100) NOT NULL,
+    id_u SERIAL NOT NULL,
+    first_name varchar(100) NOT NULL,
+    fam_name varchar(100) NOT NULL,
     email varchar(100) NOT NULL,
+    password varchar(100) NOT NULL,
     created  DATE DEFAULT NULL,
-    PRIMARY KEY (id)
+    id_challengge_compl SERIAL NOT NULL
+    
+    PRIMARY KEY (id_u)
+    FOREIGN KEY(id_u) REFERENCES Accs(id_a)
+    FOREIGN KEY(id_challengge_compl) REFERENCES CH_completed(id_comp)
+    
+);
+CREATE TABLE Clearance
+(
+    id_c SERIAL NOT NULL,
+    clearance_position varchar(10) NOT NULL
+    PRIMARY KEY(id_c)
+);
+
+
+CREATE TABLE CH_completed
+(
+    id_comp SERIAL NOT NULL,
+    created_by varchar(100) NOT NULL
+    PRIMARY KEY(id_comp)
 );
