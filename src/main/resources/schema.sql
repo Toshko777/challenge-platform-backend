a79123b3766ddf0b88b7be0449916f224f3082d3
Todor <todor.r.penchev@gmail.com>
2023-01-05 23:44:27 +0200

    comentar

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,15 +1,15 @@
-DROP TABLE IF EXISTS Accs;
+-- DROP TABLE IF EXISTS Accs;
 DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS Clearance;
-DROP TABLE IF EXISTS CH_completed;
-CREATE TABLE Accs
-(
-    id_a SERIAL NOT NULL,
-    username varchar(100) NOT NULL,
-    clearance_lvl int NOT NULL
-    PRIMARY KEY (id_a)
-    FOREIGN KEY(clearance_lvl) REFERENCES Clearance(id_c)
-);
+-- DROP TABLE IF EXISTS Clearance;
+-- DROP TABLE IF EXISTS CH_completed;
+-- CREATE TABLE Accs
+-- (
+--     id_a SERIAL NOT NULL,
+--     username varchar(100) NOT NULL,
+--     clearance_lvl int NOT NULL
+--     PRIMARY KEY (id_a)
+--     FOREIGN KEY(clearance_lvl) REFERENCES Clearance(id_c)
+-- );
 CREATE TABLE users
 (
     id_u SERIAL NOT NULL,
@@ -21,21 +21,21 @@ CREATE TABLE users
     id_challengge_compl SERIAL NOT NULL
     
     PRIMARY KEY (id_u)
-    FOREIGN KEY(id_u) REFERENCES Accs(id_a)
-    FOREIGN KEY(id_challengge_compl) REFERENCES CH_completed(id_comp)
+    -- FOREIGN KEY(id_u) REFERENCES Accs(id_a)
+    -- FOREIGN KEY(id_challengge_compl) REFERENCES CH_completed(id_comp)
     
 );
-CREATE TABLE Clearance
-(
-    id_c SERIAL NOT NULL,
-    clearance_position varchar(10) NOT NULL
-    PRIMARY KEY(id_c)
-);
+-- CREATE TABLE Clearance
+-- (
+--     id_c SERIAL NOT NULL,
+--     clearance_position varchar(10) NOT NULL
+--     PRIMARY KEY(id_c)
+-- );
 
 
-CREATE TABLE CH_completed
-(
-    id_comp SERIAL NOT NULL,
-    created_by varchar(100) NOT NULL
-    PRIMARY KEY(id_comp)
-);
+-- CREATE TABLE CH_completed
+-- (
+--     id_comp SERIAL NOT NULL,
+--     created_by varchar(100) NOT NULL
+--     PRIMARY KEY(id_comp)
+-- );
