d8ba21286e5734c74c35dc643c039fc709691903
Todor <todor.r.penchev@gmail.com>
2023-01-10 21:52:47 +0200

    neshto

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,7 @@
 INSERT INTO users(first_name, fam_name, password, email,  created, id_challengge_compl)
-values ('test', 'testov', 'test@test.com', 'parola123','2022-12-12', 1);
+values ('test', 'testov', 'test@test.com', 'parola123','2022-12-12', 1); 
+
+
 
 -- INSERT INTO Accs(username, clearance_lvl)
 -- values('test_1', 1);
